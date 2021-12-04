79a862aa57aa50929384951e475b561e2c6a6b4e
Naveen P <naveenpatke@gmail.com>
2021-12-04 13:16:46 +0530

    initial commit

---
+++ src/main/java/com/ayu/bookmymovie/BookmymovieApplication.java
@@ -0,0 +1,14 @@
+package com.ayu.bookmymovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmymovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmymovieApplication.class, args);
+	}
+
+}
+
